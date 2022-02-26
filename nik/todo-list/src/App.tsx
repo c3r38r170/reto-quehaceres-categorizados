bb00a9f98fcd0b26b37a00b5c09fc8afa7a1eafb
NicolasMangusi <mangusinicolas@gmail.com>
2022-02-26 20:36:03 -0300

    started filter

--- nik/todo-list/src/App.tsx
+++ nik/todo-list/src/App.tsx
@@ -29,133 +29,7 @@ import FormControl from '@mui/material/FormControl'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
 import Button from '@mui/material/Button'
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline'
-
-const top100Films = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    year: 2003,
-  },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: 2001,
-  },
-  {
-    title: 'Star Wars: Episode V - The Empire Strikes Back',
-    year: 1980,
-  },
-  { title: 'Forrest Gump', year: 1994 },
-  { title: 'Inception', year: 2010 },
-  {
-    title: 'The Lord of the Rings: The Two Towers',
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: 'Goodfellas', year: 1990 },
-  { title: 'The Matrix', year: 1999 },
-  { title: 'Seven Samurai', year: 1954 },
-  {
-    title: 'Star Wars: Episode IV - A New Hope',
-    year: 1977,
-  },
-  { title: 'City of God', year: 2002 },
-  { title: 'Se7en', year: 1995 },
-  { title: 'The Silence of the Lambs', year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: 'Life Is Beautiful', year: 1997 },
-  { title: 'The Usual Suspects', year: 1995 },
-  { title: 'Léon: The Professional', year: 1994 },
-  { title: 'Spirited Away', year: 2001 },
-  { title: 'Saving Private Ryan', year: 1998 },
-  { title: 'Once Upon a Time in the West', year: 1968 },
-  { title: 'American History X', year: 1998 },
-  { title: 'Interstellar', year: 2014 },
-  { title: 'Casablanca', year: 1942 },
-  { title: 'City Lights', year: 1931 },
-  { title: 'Psycho', year: 1960 },
-  { title: 'The Green Mile', year: 1999 },
-  { title: 'The Intouchables', year: 2011 },
-  { title: 'Modern Times', year: 1936 },
-  { title: 'Raiders of the Lost Ark', year: 1981 },
-  { title: 'Rear Window', year: 1954 },
-  { title: 'The Pianist', year: 2002 },
-  { title: 'The Departed', year: 2006 },
-  { title: 'Terminator 2: Judgment Day', year: 1991 },
-  { title: 'Back to the Future', year: 1985 },
-  { title: 'Whiplash', year: 2014 },
-  { title: 'Gladiator', year: 2000 },
-  { title: 'Memento', year: 2000 },
-  { title: 'The Prestige', year: 2006 },
-  { title: 'The Lion King', year: 1994 },
-  { title: 'Apocalypse Now', year: 1979 },
-  { title: 'Alien', year: 1979 },
-  { title: 'Sunset Boulevard', year: 1950 },
-  {
-    title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
-    year: 1964,
-  },
-  { title: 'The Great Dictator', year: 1940 },
-  { title: 'Cinema Paradiso', year: 1988 },
-  { title: 'The Lives of Others', year: 2006 },
-  { title: 'Grave of the Fireflies', year: 1988 },
-  { title: 'Paths of Glory', year: 1957 },
-  { title: 'Django Unchained', year: 2012 },
-  { title: 'The Shining', year: 1980 },
-  { title: 'WALL·E', year: 2008 },
-  { title: 'American Beauty', year: 1999 },
-  { title: 'The Dark Knight Rises', year: 2012 },
-  { title: 'Princess Mononoke', year: 1997 },
-  { title: 'Aliens', year: 1986 },
-  { title: 'Oldboy', year: 2003 },
-  { title: 'Once Upon a Time in America', year: 1984 },
-  { title: 'Witness for the Prosecution', year: 1957 },
-  { title: 'Das Boot', year: 1981 },
-  { title: 'Citizen Kane', year: 1941 },
-  { title: 'North by Northwest', year: 1959 },
-  { title: 'Vertigo', year: 1958 },
-  {
-    title: 'Star Wars: Episode VI - Return of the Jedi',
-    year: 1983,
-  },
-  { title: 'Reservoir Dogs', year: 1992 },
-  { title: 'Braveheart', year: 1995 },
-  { title: 'M', year: 1931 },
-  { title: 'Requiem for a Dream', year: 2000 },
-  { title: 'Amélie', year: 2001 },
-  { title: 'A Clockwork Orange', year: 1971 },
-  { title: 'Like Stars on Earth', year: 2007 },
-  { title: 'Taxi Driver', year: 1976 },
-  { title: 'Lawrence of Arabia', year: 1962 },
-  { title: 'Double Indemnity', year: 1944 },
-  {
-    title: 'Eternal Sunshine of the Spotless Mind',
-    year: 2004,
-  },
-  { title: 'Amadeus', year: 1984 },
-  { title: 'To Kill a Mockingbird', year: 1962 },
-  { title: 'Toy Story 3', year: 2010 },
-  { title: 'Logan', year: 2017 },
-  { title: 'Full Metal Jacket', year: 1987 },
-  { title: 'Dangal', year: 2016 },
-  { title: 'The Sting', year: 1973 },
-  { title: '2001: A Space Odyssey', year: 1968 },
-  { title: "Singin' in the Rain", year: 1952 },
-  { title: 'Toy Story', year: 1995 },
-  { title: 'Bicycle Thieves', year: 1948 },
-  { title: 'The Kid', year: 1921 },
-  { title: 'Inglourious Basterds', year: 2009 },
-  { title: 'Snatch', year: 2000 },
-  { title: '3 Idiots', year: 2009 },
-  { title: 'Monty Python and the Holy Grail', year: 1975 },
-]
+import Menu from '@mui/material/Menu'
 
 
 const themeLight = createTheme({
@@ -219,7 +93,17 @@ function App() {
   //List
   const [list, setList] = useState<iTask[]>([])
   const [completedList, setCompletedList] = useState<iTask[]>([])
-  const [selected, setSelected] = useState(false);
+  //Filters
+  const [filter, setFilter] = useState('')
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const openFilterMenu = Boolean(anchorEl)
+  const handleClickFilterMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  }
+  const handleFilter = (filter: string) => {
+    setFilter(filter)
+    setAnchorEl(null)
+  }
   //Misc
   const [light, setLight] = useState(true)
   const [search, setSearch] = useState('')
@@ -278,9 +162,9 @@ function App() {
                   label="Type"
                   onChange={handleChangeType}
                 >
-                  <MenuItem value={'Work'}>Work</MenuItem>
-                  <MenuItem value={'University'}>University</MenuItem>
-                  <MenuItem value={'Other'}>Other</MenuItem>
+                  <MenuItem value='Work'>Work</MenuItem>
+                  <MenuItem value='University'>University</MenuItem>
+                  <MenuItem value='Other'>Other</MenuItem>
                 </Select>
               </FormControl>
               <DatePicker
@@ -306,16 +190,30 @@ function App() {
         </Modal>
         <Grid className="main" sx={{padding: 2}}>
           <Stack direction="row" spacing={2}>
-            <Autocomplete
-              id="search-bar"
-              freeSolo
-              options={list.map((task) => task.taskName)}
-              renderInput={(params) => <TextField {...params} label="Search task" value={search} onChange={(event) => setSearch(event.target.value)}/>}
-              sx={{width: '95%'}}
-            />
-            <IconButton color="primary" aria-label="filter-list" sx={{borderRadius: '20%', width: 50}}>
+            <TextField label="Search task" value={search} onChange={(event) => setSearch(event.target.value)} sx={{width: '95%'}}/>
+            <IconButton 
+              color="primary" aria-label="filter-list" sx={{borderRadius: '20%', width: 50}}
+              id="basic-button"
+              aria-controls={openFilterMenu ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={openFilterMenu ? 'true' : undefined}
+              onClick={handleClickFilterMenu}
+            >
               <FilterListIcon/>
             </IconButton>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={openFilterMenu}
+              onClose={() => setAnchorEl(null)}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <MenuItem onClick={() => handleFilter('Work')}>Work</MenuItem>
+              <MenuItem onClick={() => handleFilter('University')}>University</MenuItem>
+              <MenuItem onClick={() => handleFilter('Other')}>Other</MenuItem>
+            </Menu>
             <IconButton color="primary" aria-label="add-task" sx={{borderRadius: '20%', width: 50}} onClick={() => setOpenModal(true)}>
               <AddBoxIcon/>
             </IconButton>
@@ -331,7 +229,7 @@ function App() {
           </Grid>
           <Grid
           >
-            {list.filter((task)=>{
+            {list.filter((task) => {
               if(search === ''){
                 return task
               } else if (task.taskName.toLowerCase().includes(search.toLowerCase())) {
@@ -390,9 +288,9 @@ function App() {
                     }
                     action={
                       <>
-                        <IconButton onClick={()=>handleMoveToActive(index, task.id)}>
+                        {/* <IconButton onClick={()=>handleMoveToActive(index, task.id)}>
                           <CheckIcon/>
-                        </IconButton>
+                        </IconButton> */}
                         <IconButton onClick={()=> setCompletedList(completedList.filter(item => item.id !== task.id))}>
                           <RemoveCircleOutlineIcon/>
                         </IconButton>
